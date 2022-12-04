8fb83c834693110f6e4e051b960ac1facbdeef29
Hunain Parekh <hunainparekh85@gmail.com>
2022-12-04 20:20:15 +0500

    AC:02 Test Pass by Hunain

--- src/Cart.js
+++ src/Cart.js
@@ -4,5 +4,15 @@ module.exports = class Cart {
         this.items = [];
         this.totalPrice = 0;
     }
+
+    addItem(item,quantity){
+        this.items.push({
+            itemName : item.name,
+            price: item.price,
+            quantity: quantity,
+            onSale : item.onSale
+        });
+        this.totalPrice += item.price * quantity;
+    }
 }
  
--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,11 +1,26 @@
-const Cart = require('../src/Cart.js');
+const Cart = require('../src/Cart');
+const Item = require('../src/Item');
 const expect = require('chai').expect;
 
 describe('Cart', () => {
     //Given that I visit the site, when I begin shopping, then I expect my cart to be empty.
     it('should initialize as empty', () => {
+        //Arrange
         const cart = new Cart();
+        //Assert
         expect(cart.items).to.deep.equal([])
         expect(cart.totalPrice).to.be.equal(0)
+    });
+
+    //Given I have an empty cart, when I add an Item, then I expect to see `totalPrice` reflect the sum of all the Items in my cart,
+    // times the quantities of each item.
+    it('should be the change of totalPrice after adding item depends on item quantity', ()=>{
+        //Arrange
+        const cart = new Cart();
+        const item = new Item('Book',200,false);
+        //Act
+        cart.addItem(item,5);
+        //Assert
+        expect(cart.totalPrice).to.be.equal(1000);
     });
 });
